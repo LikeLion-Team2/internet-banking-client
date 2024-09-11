684c74124e0bba034613fabee48dd824b1babb6e
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-09-11 15:44:38 +0900

    chore: vite 설정 파일 수정 프록시 타겟 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+``;
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
       "/api": {
-        target: "https://localhost:8080",
+        target: "https://bank-back:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
